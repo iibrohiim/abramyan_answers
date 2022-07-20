bae2413e00a51922a6237eadc680e853700e10ad
green-thunder <kamolovnodirbek1@gmail.com>
2022-07-20 14:10:57 +0500

    new file

---
+++ func/56.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+double Leng(double x1,double y1, double x2,double y2){
+    return sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+}
+
+int main(){
+    double x1,x2,y1,y2;
+    while(1){
+        printf("A(x,y)=");
+        scanf("%lf,%lf",&x1,&y1);
+        printf("B(x,y)=");
+        scanf("%lf,%lf",&x2,&y2);
+        printf("|AB|=%lf\n\n",Leng(x1,y1,x2,y2));
+    }
+    return 0;
+}
